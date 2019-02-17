07a3c23cc94e67a762ebb4dbd6555120b99c2e83
CZ <czavaleta@gbtrealty.com>
2019-02-17 12:23:24 -0600

    added more exs

--- exercs/ex5.rb
+++ exercs/ex5.rb
@@ -10,7 +10,7 @@ puts "Let's talk bout #{my_name}."
 puts "He's #{my_height} inches tall."
 puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair hair}."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
-
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
 
